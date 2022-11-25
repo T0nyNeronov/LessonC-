751149c4bbd99fa5dc26761f770d43ef625e725a
Antony <ashout10@gmail.com>
2022-11-25 15:53:28 +0300

    1 задача семинара

--- Semi_1/Program.cs
+++ Semi_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// //  Напишите программу, которая принимает 
+// на вход ичсло (А) и выдает сумму чисел от 1 до А
+
+Console.WriteLine("Add number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(getsumnum(num));
+
+int getsumnum (int res)
+{
+    int sum =0;
+    int count =0;
+
+    while(count<res)
+    {
+        count++;
+        sum +=count; 
+        
+    }
+    return sum;
+}

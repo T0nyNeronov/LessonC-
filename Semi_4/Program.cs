c0a11c7768e3d3132fd057e85b2a5cea655c1554
Antony <ashout10@gmail.com>
2022-11-25 22:54:12 +0300

    Задание 4(решение с массивом)

---
+++ Semi_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Console.WriteLine("Add number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int [] getArray(int size)
+{
+    int [] array1 = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array1[i] = new Random().Next(0,2);
+    }
+    
+    return array1;
+
+}
+int [] array1 = getArray(8);
+
+Console.WriteLine(string.Join(",",array1));

7ca980ea4f30c8dc923fe9387bb88c0e8746260a
Antony <ashout10@gmail.com>
2022-11-25 22:02:51 +0300

    Задание 3

---
+++ Semi_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// // Напишите программу, которая принимает на вход число N 
+// // и выдает произведение чисел от 1 до N.
+// 4->24
+// 5->120
+
+Console.WriteLine("Add number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Произведение  "+getsumnum(num));
+
+int getsumnum (int res)
+{
+    
+    int count =1;
+    int result = 1;
+
+    while(res>=count)
+    {
+        result*=count; 
+        count++;
+    }
+return result;
+} 

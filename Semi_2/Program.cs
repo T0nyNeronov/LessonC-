07a7f64865e26cafe684e494dc275ce8007fbbc1
Antony <ashout10@gmail.com>
2022-11-25 16:24:08 +0300

    Задача 2

---
+++ Semi_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдает количество цифп в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Add number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Количество цифр "+getsumnum(num));
+
+int getsumnum (int res)
+{
+    int count =0;
+
+    while(res>0)
+    {
+        count++;
+        res/=10; 
+        
+    }
+return count;
+}

9cc0a5d97680e44a6b55630bb6306ba071d6cbed
Antony <ashout10@gmail.com>
2022-11-26 14:16:02 +0300

    Цикл For

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -6,10 +6,30 @@
 // // 2, 4 -> 16
 
 
+// Console.WriteLine("Add number one");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Add number two");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Итоговый результат = " + Math.Pow(num1, num2));
+
+
 Console.WriteLine("Add number one");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Add number two");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Итоговый результат = " + Math.Pow(num1, num2));
+int GetNumCube(int res)
+{
+    int result=1;
+for (int i = 1; i <= num2; i++)
+{
+result=result *num1;
+  
+}
+return result;
+}
+
+Console.WriteLine(num1+" в степени "+num2+" равно "+GetNumCube(num1));

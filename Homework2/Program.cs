0c69615aa97ed25604cd2c5a0a0b3d14c0184f1b
Antony <ashout10@gmail.com>
2022-11-26 15:24:49 +0300

    Задание2

---
+++ Homework2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetFiguresSum(int res)
+{
+    int sum=0;
+   while (res>0)
+    {
+        sum=sum+res%10;
+        res/=10;
+    }
+    return sum;
+    }
+    Console.WriteLine("сумма цифр числа "+num+" равна "+GetFiguresSum(num));
+
+            
